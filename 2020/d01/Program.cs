d7bb1e13842e52473e151d809cd1c506452882e2
Wouter Lindenhof <wouterlindenhof@gmail.com>
2021-11-29 10:24:24 +0100

    Add day 1 of 2020

---
+++ 2020/d01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+static class Program
+{
+    static void Main()
+    {
+
+        var input = File.ReadAllLines("input.txt").Select(x=>int.Parse(x)).ToArray();
+
+        for (var i = 0; i < input.Length; ++i)
+        {
+            for (var j = (i + 1); j < input.Length; ++j)
+            {
+                var a = input[i];
+                var b = input[j];
+                if ((a + b) == 2020)
+                {
+                    Console.WriteLine($"{a}+{b} = {a + b}, {a}*{b}={a * b}");
+                }
+
+                for (var k = (j + 1); k < input.Length; ++k)
+                {
+                    var c = input[k];
+                    if ((a + b + c) == 2020)
+                    {
+                        Console.WriteLine($"{a}+{b}+{c} = {a + b + c}, {a}*{b}*{c}={a * b * c}");
+                    }
+                }
+            }
+        }
+    }
+}
+
